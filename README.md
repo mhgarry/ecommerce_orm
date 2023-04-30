9629c2ed9ed3d8fe6f9f79bce470b5ef7752da7c
Matt <mhgarry92@gmail.com>
2023-04-30 01:15:44 -0400

    producttag models

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## sources
 https://medium.com/geekculture/how-i-built-an-e-commerce-api-with-nodejs-express-and-mongodb-part-4-318e3f494611
 https://blog.logrocket.com/crud-rest-api-node-js-express-postgresql/
+https://sequelize.org/docs/v6/core-concepts/model-basics/#:~:text=Models%20are%20the%20essence%20of,(and%20their%20data%20types).
+https://sequelize.org/docs/v6/core-concepts/model-querying-basics/
+https://dev.to/mcarpenter/sequelize-model-guide-5efk
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    // tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -14,7 +36,7 @@ ProductTag.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
+  },
 );
 
 module.exports = ProductTag;
