1c092f7ea833a5b0794f33175c332e473e81112f
Matt <mhgarry92@gmail.com>
2023-04-30 06:01:23 -0400

    get single tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,27 +7,43 @@ router.get('/', async (req, res) => {
   try {
     const dbTagData = await Tag.findAll({
       attributes: ['id', 'tag_name'],
-      include: [
-        {
+      include: [{
           model: Product,
           attributes: ['id', 'product_name', 'price',
             'stock', 'category_id'],
         },
-      ],
+			],
     });
     res.json(dbTagData);
-    console.log(dbTagData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+	try {
+		const dbTagData = await Tag.findOne({
+			where: {
+				id: req.params.id,
+			},
+			attributes: ['id', 'tag_name'],
+			include: [{
+				model: Product,
+				attricutes: ['id', 'product_name', 'price',
+			'stock', 'category_id'],
+			},
+		],
+	});
+		if (!dbTagData) {
+			return res.status(404).json({ message: 'No tag found with this id.'});
+		}
+		res.json(dbTagData);
+		} catch (err) {
+			console.log(err);
+			res.status(500).json(err);
+		}
 });
-
 router.post('/', (req, res) => {
   // create a new tag
 });
